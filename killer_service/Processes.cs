5a577dd08a397a1c37093b70c9833177ffa08ebe
Denis Bystrov (OpenDev) <denis.bystrov@opendev.com>
2019-12-04 11:52:58 +0400

    code

---
+++ killer_service/Processes.cs
@@ -0,0 +1,82 @@
+﻿/*
+ * User: BystrovDM
+ */
+
+using System;
+using System.Linq;
+using System.Diagnostics;
+using NLog;
+
+namespace killer_service
+{
+    public static class Processes
+    {
+        private static readonly Logger Log = LogManager.GetCurrentClassLogger();
+
+        public static void show_all_process()
+        {
+            var allProcess = from proc in Process.GetProcesses(".")
+                orderby proc.Id
+                select proc;
+
+            foreach (var proc in allProcess)
+            {
+                try
+                {
+                    Console.WriteLine(@"PID:{0}; Name:{1}; StartTime:{2}", proc.Id, proc.ProcessName, proc.StartTime);
+                }
+                catch (Exception e)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine(@"ERROR: {0}: {1}", e.Message, proc.ProcessName);
+                    Console.ForegroundColor = ConsoleColor.Gray;
+                }
+            }
+        }
+
+        public static void show_process(string procName)
+        {
+            var allProcess = from proc in Process.GetProcesses(".")
+                orderby proc.Id
+                where proc.ProcessName.Equals(procName)
+                select proc;
+
+            foreach (var proc in allProcess)
+            {
+                try
+                {
+                    Console.WriteLine(@"PID:{0}; Name:{1}; StartTime:{2}", proc.Id, proc.ProcessName, proc.StartTime);
+                }
+                catch (Exception e)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine(@"ERROR: {0}: {1}", e.Message, proc.ProcessName);
+                    Console.ForegroundColor = ConsoleColor.Gray;
+                }
+            }
+        }
+
+
+        public static void kill_old_process(string procName, int liveDuration)
+        {
+            var allProcess = from proc in Process.GetProcesses(".")
+                orderby proc.Id
+                where proc.ProcessName.Equals(procName) && proc.StartTime.AddMinutes(liveDuration) < DateTime.Now
+                select proc;
+
+            foreach (var proc in allProcess)
+            {
+                //if (DateTime.Now > proc.StartTime.AddMinutes(live_duration)) 
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine(@"Kill process: pid:{0}; Name: {1}; StartTime: {2}", proc.Id, proc.ProcessName,
+                        proc.StartTime);
+                    Console.ForegroundColor = ConsoleColor.Gray;
+                    proc.Kill();
+                    Log.Info(@"Kill process: pid:{0}; Name: {1}; StartTime: {2}.", proc.Id, proc.ProcessName,
+                        proc.StartTime);
+                }
+            }
+        }
+    }
+}
---
+++ killer_service/Program.cs
@@ -0,0 +1,60 @@
+﻿/*
+ * User: BystrovDM
+ */
+
+using System;
+using System.Timers;
+using NLog;
+
+namespace killer_service
+{
+    internal static class Program
+    {
+        private static string _processName;
+        private static int _live;
+        private static int _interval;
+
+        private static readonly Logger Log = LogManager.GetCurrentClassLogger();
+
+        public static void Main(string[] args)
+        {
+            if (args != null && args.Length == 3)
+            {
+                var flag = true;
+                const string stopFlag = "q";
+
+                Console.WriteLine("Enter '{0}' to exit.", stopFlag);
+
+                _processName = args[0];
+                _live = Convert.ToInt16(args[1]);
+                _interval = Convert.ToInt16(args[2]);
+
+                Log.Info("Run program at {0}. Search process: '{1}'. Max Live: {2} min. Interval check: {3} min.",
+                    DateTime.Now, _processName, _live, _interval);
+
+                var timer = new System.Timers.Timer(60 * 1000 * _interval);
+                timer.Elapsed += OnTimeout;
+                timer.AutoReset = true;
+                timer.Enabled = true;
+
+                do
+                {
+                    var key = Console.ReadLine();
+                    if (key != null) flag &= !key.Equals(stopFlag);
+                } while (flag);
+            }
+
+            Console.WriteLine("Program is shutdown.");
+            Log.Info("Program is shutdown at {0}.", DateTime.Now);
+        }
+
+        private static void OnTimeout(Object source, ElapsedEventArgs e)
+        {
+            var curTime = DateTime.Now;
+            Console.WriteLine("Current time: {0}", curTime);
+
+            Processes.show_process(_processName);
+            Processes.kill_old_process(_processName, _live);
+        }
+    }
+}
